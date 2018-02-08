b1f781ff5c9ce5d9550d702a1102d6b8945d2fca
jiangyidong <me.mory@qq.com>
2018-02-08 19:22:18 +0800

    [update]调整布局

--- app/src/main/cpp/VideoPlayer.cpp
+++ app/src/main/cpp/VideoPlayer.cpp
@@ -24,6 +24,7 @@ extern "C"{
 
     // sd卡中的视频文件地址,可自行修改或者通过jni传入
     char *file_name = "/storage/emulated/0/DCIM/Camera/VID_20180203_120655.mp4";
+//    char *file_name = "/storage/sdcard0/DCIM/Camera/VID_20180203_120655.mp4";
 
     av_register_all();
 
